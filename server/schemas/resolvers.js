cdab35addaf3e66aa07259c935fc6816880bc25d
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-15 17:04:06 -0700

    resolvers and mutations written

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 // import AuthenticationError npm
 const { AuthenticationError } = require('apollo-server-express');
 //import User from /models/User
-const { User } = require('../models');
+const { User, Book } = require('../models');
 
 // imported from /utils/auth.js
 const { signToken } = require('../utils/auth');
@@ -14,7 +14,37 @@ const resolvers = {
 
         user: async (parent, { userId }) => {
             return User.findOne({ _id: userId})
+        },
 
-        }
+        me: async (parent, args, context ) => {
+            if(context.user) {
+                return User.findOne({ _id: context.user._id }).populate('books');       
+            }
+            throw new AuthenticationError('Please login to view your books!');
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }
+        },
+        login: async (parent, {email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Username and password do not match, try again');
+            }
+
+           const correctPw = await user.isCorrectPassword(password)
+
+           if (!correctPw) {
+            throw new AuthenticationError('Username and password do not match, try again')
+           }
+
+           const token = signToken(user)
+           return { token, user }
+        },
+        addBook: async (parent, { })
     }
 }
